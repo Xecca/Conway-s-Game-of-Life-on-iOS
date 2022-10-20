954b6357e4400973680a92a942a2c8c1d90cfcc7
Xecca <ensimgen@gmail.com>
2022-10-20 18:56:58 +0300

    initial commit.

---
+++ Conway-s-Game-of-Life-on-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Conway-s-Game-of-Life-on-iOS
+//
+//  Created by Dreik on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
+    }
+}
+
